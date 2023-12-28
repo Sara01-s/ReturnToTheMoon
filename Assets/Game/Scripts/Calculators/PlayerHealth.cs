a575dba4a43669e873ba28d803c7a8ee5c9e222f
Master00021 <107310643+Master00021@users.noreply.github.com>
2023-12-28 08:30:06 -0300

    Error compilacion PlayerHealth arreglado

--- Assets/Game/Scripts/Calculators/PlayerHealth.cs
+++ Assets/Game/Scripts/Calculators/PlayerHealth.cs
@@ -5,12 +5,7 @@ namespace Game {
     
     internal sealed class PlayerHealth : MonoBehaviour { // TODO
        
-        internal ReactiveProperty<float> CurrentHealth; 
-
-        private float MAX_HEALTH;
-
         private void Awake() {
-            CurrentHealth = MAX_HEALTH;
         }
 
         private void OnEnable() {
