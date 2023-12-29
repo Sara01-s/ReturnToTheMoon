421809904c7c52ce5cc8460f88bd582892c7c6c9
Master00021 <107310643+Master00021@users.noreply.github.com>
2023-12-29 08:57:07 -0300

    Distance Calculator

--- Assets/Game/Scripts/Calculators/DistanceCalculator.cs
+++ Assets/Game/Scripts/Calculators/DistanceCalculator.cs
@@ -4,6 +4,19 @@ namespace Game {
     
     internal sealed class DistanceCalculator : MonoBehaviour {
         
+        [SerializeField] private FloatResourceRx _currentDistance;
+        
+        [SerializeField] private Transform _start;
+        [SerializeField] private Transform _player;
+
+        private void Update() {
+            UpdateCurrentDistance();
+        }
+
+        private void UpdateCurrentDistance() {
+            _currentDistance.Value = _player.position.x - _start.position.x;
+            print(_currentDistance.Value);
+        }
 
 
     }
